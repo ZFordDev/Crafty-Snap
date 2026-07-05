8afc829cc17c555a6a040661e576c8bd333fa9cb
ZFordDev <zforddev@gmail.com>
2026-07-05 16:08:08 +1000

    basic print

---
+++ src/crafty_snap/__main__.py
@@ -0,0 +1,7 @@
+def main():
+    print("Crafty-Snap: R&D for Snapcraft packaging.")
+    print("Nothing works yet, but it's a start.")
+    print("Stay tuned...")
+
+if __name__ == "__main__":
+    main()
